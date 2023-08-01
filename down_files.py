4e0008c8838437fff95a2f933d5de1c691cf383c
Feisal01 <feisal_darar@hotmail.com>
2023-08-01 15:45:59 -0400

    updates

--- down_files.py
+++ down_files.py
@@ -4,11 +4,17 @@ import os
 with open ('linkFile.txt') as f:
     lines = [line.rstrip('\n') for line in f]
 f.close
+new_filename = ["openDataPortal.siteAnalytics.datasetsByOrg.bilingual.csv","openDataPortal.siteAnalytics.datasetsByOrgByMonth.bilingual.csv",
+                "openDataPortal.siteAnalytics.internationalUsageBreakdown.bilingual.csv", "openDataPortal.siteAnalytics.provincialUsageBreakdown.bilingual.csv",
+                "openDataPortal.siteAnalytics.top100Datasets.bilingual.csv","openDataPortal.siteAnalytics.totalMonthlyUsage.bilingual.csv"]
 # CSV files downloading method / URL
 def filedow (reqURL):
     try:
         req = requests.get(reqURL)
-        filename = reqURL.split('/')[-1]
+        filename =  reqURL.split('/')[-1]
+        for new_name in new_filename:
+            if new_name.lower() == filename:
+                filename = new_name
         with open (filename, 'wb') as f:
             for chunk in req.iter_content(chunk_size=1024):
                 if chunk:
@@ -24,3 +30,4 @@ def filedownload():
 def csv_files_remove():
     for element in lines:
      os.remove(element.split('/')[-1])
+filedownload()
